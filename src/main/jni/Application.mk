8224dfb81949af85e88b6ea80ecb8500784052fa
mpraveen <mpraveen@vgyanitservices.com>
2025-09-04 16:32:50 +0530

    - updated sdk versions - Fixed 16kb memory page size issue

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,9 +4,10 @@ APP_CPPFLAGS += -fexceptions
 #For ANativeWindow support
 APP_PLATFORM = android-14
 
-APP_ABI :=  armeabi-v7a \
-            arm64-v8a \
-            mips \
-            mips64 \
+# Support for 16KB page size devices
+APP_CPPFLAGS += -DSUPPORT_16KB_PAGES
+
+APP_ABI :=  arm64-v8a \
+            armeabi-v7a \
             x86 \
             x86_64
